eab0656940424fa17883981d4233efda9ddd87cc
MrFor <forforsen@gmail.com>
2018-12-25 02:04:29 +0700

    add style

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    path(r'^post/new/$', views.post_new, name='post_new'),
-    path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    path(r'post/new/$', views.post_new, name='post_new'),#path(r'^post/new/$', views.post_new, name='post_new'),
+    path(r'post/<int:pk>/edit/$', views.post_edit, name='post_edit'),#path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
