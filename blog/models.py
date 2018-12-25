c5e6350bc9d3ca05dbe26b2dc6d4410a4846e4bb
MrFor <forforsen@gmail.com>
2018-12-25 23:58:02 +0700

    add russ in admin

--- blog/models.py
+++ blog/models.py
@@ -3,13 +3,13 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=400)
-    text = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name=u"Пользователь")
+    title = models.CharField(u'Заголовко', max_length=400)
+    text = models.TextField(u'Текст')
     created_date = models.DateTimeField(
-            default=timezone.now)
+            default=timezone.now, verbose_name=u"Дата создания")
     published_date = models.DateTimeField(
-            blank=True, null=True)
+            blank=True, null=True, verbose_name=u"Дата публикации")
 
 
     def publish(self):
@@ -34,11 +34,11 @@ class Tik(models.Model):
     def __str__(self):
         return self.title
 class Dubrovka(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=400)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name=u"Пользователь")
+    title = models.CharField(u'Имя файла',max_length=400)
     created_date = models.DateTimeField(
-            default=timezone.now)
-    media_file = models.FileField(upload_to='user_media')
+            default=timezone.now, verbose_name=u"Дата создания")
+    media_file = models.FileField(upload_to='user_media', verbose_name=u"Загрузить файл")
     def publish(self):
         self.published_date = timezone.now()
         self.save()
