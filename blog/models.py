836d86bd125bedec74accbc218a970f389711386
MrFor <forforsen@gmail.com>
2018-12-26 11:35:56 +0700

    add ru in admin panel

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name=u"Пользователь")
-    title = models.CharField(u'Заголовко', max_length=400)
+    title = models.CharField(u'Заголовок', max_length=400)
     text = models.TextField(u'Текст')
     created_date = models.DateTimeField(
             default=timezone.now, verbose_name=u"Дата создания")
